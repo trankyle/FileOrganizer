2d96dc3b4b57b6d3eca189832d9c1bb4a04449f7
trankyle <Kyle_tran@outlook.com>
2024-12-06 03:01:44 -0700

    Add project files.

---
+++ FileOrganizerUtility/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblTitle = New Label()
+        btnSelectFolder = New Button()
+        txtFolderPath = New TextBox()
+        listViewFiles = New ListView()
+        btnOrganize = New Button()
+        SuspendLayout()
+        ' 
+        ' lblTitle
+        ' 
+        lblTitle.AutoSize = True
+        lblTitle.Font = New Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTitle.Location = New Point(132, 9)
+        lblTitle.Name = "lblTitle"
+        lblTitle.Size = New Size(209, 25)
+        lblTitle.TabIndex = 0
+        lblTitle.Text = "Organize Files by Type"
+        lblTitle.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' btnSelectFolder
+        ' 
+        btnSelectFolder.Location = New Point(12, 52)
+        btnSelectFolder.Name = "btnSelectFolder"
+        btnSelectFolder.Size = New Size(75, 23)
+        btnSelectFolder.TabIndex = 1
+        btnSelectFolder.Text = "Select Folder"
+        btnSelectFolder.UseVisualStyleBackColor = True
+        ' 
+        ' txtFolderPath
+        ' 
+        txtFolderPath.Location = New Point(93, 52)
+        txtFolderPath.Name = "txtFolderPath"
+        txtFolderPath.ReadOnly = True
+        txtFolderPath.Size = New Size(369, 23)
+        txtFolderPath.TabIndex = 2
+        ' 
+        ' listViewFiles
+        ' 
+        listViewFiles.Location = New Point(12, 81)
+        listViewFiles.Name = "listViewFiles"
+        listViewFiles.Size = New Size(450, 200)
+        listViewFiles.TabIndex = 3
+        listViewFiles.UseCompatibleStateImageBehavior = False
+        listViewFiles.View = View.Details
+        ' 
+        ' btnOrganize
+        ' 
+        btnOrganize.Location = New Point(198, 287)
+        btnOrganize.Name = "btnOrganize"
+        btnOrganize.Size = New Size(75, 23)
+        btnOrganize.TabIndex = 4
+        btnOrganize.Text = "Organize Files"
+        btnOrganize.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(477, 361)
+        Controls.Add(btnOrganize)
+        Controls.Add(listViewFiles)
+        Controls.Add(txtFolderPath)
+        Controls.Add(btnSelectFolder)
+        Controls.Add(lblTitle)
+        Name = "Form1"
+        Text = "File Organizer Utility"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnSelectFolder As Button
+    Friend WithEvents txtFolderPath As TextBox
+    Friend WithEvents listViewFiles As ListView
+    Friend WithEvents btnOrganize As Button
+
+End Class
---
+++ FileOrganizerUtility/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub btnSelectFolder_Click(sender As Object, e As EventArgs) Handles btnSelectFolder.Click
+        Dim folderDialog As New FolderBrowserDialog()
+
+        If folderDialog.ShowDialog() = DialogResult.OK Then
+            txtFolderPath.Text = folderDialog.SelectedPath
+            LoadFiles(folderDialog.SelectedPath)
+        End If
+    End Sub
+
+
+    Private Sub LoadFiles(folderPath As String)
+        ' Clear existing items
+        listViewFiles.Items.Clear()
+
+        ' Add columns if not already added
+        If listViewFiles.Columns.Count = 0 Then
+            listViewFiles.Columns.Add("File Name", 250)
+            listViewFiles.Columns.Add("File Type", 100)
+            listViewFiles.Columns.Add("Size (KB)", 100)
+        End If
+
+        ' Get all files in the folder
+        Dim files As String() = Directory.GetFiles(folderPath)
+
+        ' Add each file to the ListView
+        For Each file In files
+            Dim fileInfo As New FileInfo(file)
+            Dim item As New ListViewItem(fileInfo.Name)
+            item.SubItems.Add(fileInfo.Extension)
+            item.SubItems.Add((fileInfo.Length / 1024).ToString("F2"))
+            listViewFiles.Items.Add(item)
+        Next
+    End Sub
+    Private Sub btnOrganize_Click(sender As Object, e As EventArgs) Handles btnOrganize.Click
+        Dim folderPath As String = txtFolderPath.Text
+
+        If String.IsNullOrWhiteSpace(folderPath) OrElse Not Directory.Exists(folderPath) Then
+            MessageBox.Show("Please select a valid folder.")
+            Return
+        End If
+
+        ' Organize files by type
+        Dim files As String() = Directory.GetFiles(folderPath)
+
+        For Each cfile In files
+            Dim fileInfo As New FileInfo(cfile)
+            Dim fileType As String = fileInfo.Extension.TrimStart(".").ToUpper()
+            Dim destinationFolder As String = Path.Combine(folderPath, fileType)
+
+            ' Create subfolder if it doesn't exist
+            If Not Directory.Exists(destinationFolder) Then
+                Directory.CreateDirectory(destinationFolder)
+            End If
+
+            ' Move file to the subfolder
+            Dim destinationFile As String = Path.Combine(destinationFolder, fileInfo.Name)
+            File.Move(cfile, destinationFile)
+        Next
+
+        ' Refresh the ListView
+        LoadFiles(folderPath)
+        MessageBox.Show("Files organized successfully!")
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
